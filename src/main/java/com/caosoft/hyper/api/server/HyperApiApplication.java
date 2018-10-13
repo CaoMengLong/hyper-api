31f5dae519d94c21da15102f56df675849f4868e
CaoMengLong <138888611@qq.com>
2018-10-13 15:28:36 +0800

    first commit

---
+++ src/main/java/com/caosoft/hyper/api/server/HyperApiApplication.java
@@ -0,0 +1,12 @@
+package com.caosoft.hyper.api.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperApiApplication.class, args);
+    }
+}
